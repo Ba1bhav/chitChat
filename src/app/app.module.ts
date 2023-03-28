66ea05847a4d2c801c322a9d84487bb5d35d60e7
Ba1bhav <baibhav.kumar@chicmic.co.in>
2023-03-28 17:20:38 +0530

    intercepted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { SharedModule } from 'src/modules/shared/shared.module';
+import { RequestInterceptorInterceptor } from 'src/services/shared/request-interceptor.interceptor';
 @NgModule({
   declarations: [
     AppComponent
@@ -16,7 +17,7 @@ import { SharedModule } from 'src/modules/shared/shared.module';
     HttpClientModule,
     SharedModule
   ],
-  providers: [],
+  providers: [{provide:HTTP_INTERCEPTORS,useClass:RequestInterceptorInterceptor,multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
